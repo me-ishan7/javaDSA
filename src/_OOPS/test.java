36cd758c7cf626fa8513bc54134407ffe5f9401d
me-ishan7 <ik1362005@gmail.com>
2025-08-29 23:53:57 +0530

    Java

--- src/_OOPS/test.java
+++ src/_OOPS/test.java
@@ -5,8 +5,10 @@ public class test {
         Animal dog = new Animal();
         dog.color = "blue";
         dog.breed = "German Sheperd";
+        dog.legs = 4;
         dog.running(23);
-        System.out.println();
+
+        System.out.println(dog.color);
         System.out.println(dog.speed);
     }
 }
