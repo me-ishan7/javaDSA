040e0af1098d7565b3b6198bccf23fae29e62f4c
me-ishan7 <ik1362005@gmail.com>
2025-09-02 22:47:06 +0530

    Java

---
+++ src/Recursion/Palindrome.java
@@ -0,0 +1,20 @@
+package Recursion;
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number : ");
+        int n = sc.nextInt();
+        int ans = reverse(n ,0);
+        if(n == ans) {
+            System.out.println(ans + " is a palindrome number.");
+        }
+        else {
+            System.out.println(n + " is not a palindrome");
+        }
+    }
+    static int reverse(int n , int rev){
+        if(n == 0) return rev;
+        return reverse(n / 10 , rev * 10 + n % 10);
+    }
+}
