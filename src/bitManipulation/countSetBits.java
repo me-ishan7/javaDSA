51feb5a5027c9198dabcf63002109fb73ea5e616
me-ishan7 <ik1362005@gmail.com>
2025-09-25 18:05:19 +0530

    Java

---
+++ src/bitManipulation/countSetBits.java
@@ -0,0 +1,19 @@
+package bitManipulation;
+import java.util.Scanner;
+public class countSetBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number : ");
+        int n = sc.nextInt();
+        int ans = count(n);
+        System.out.println("The set bits are: "+ ans);
+    }
+    static int count(int n) {
+        int res = 0;
+        while (n > 0){
+            res = res + (n & 1);
+            n = n >> 1;
+        }
+        return res;
+    }
+}
